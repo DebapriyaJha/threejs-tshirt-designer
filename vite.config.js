fe86531d42367b5be20f5d3c62695f1dab0c7304
DebapriyaJha <111692420+DebapriyaJha@users.noreply.github.com>
2024-05-17 16:39:19 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['@mediapipe/tasks-vision', '@chevrotain/gast', '@chevrotain/cst-dts-gen', '@chevrotain/utils'],
+      output: {
+        manualChunks: {
+          'my-chunk': ['src/main.jsx', 'src/index.css', 'src/pages/Customizer.jsx', 'src/pages/Home.jsx', 'src/components/AIPicker.jsx', 
+          'src/components/ColorPicker.jsx', 'src/components/FilePicker.jsx' ],
+          // Add more chunks as needed
+        },
+      },
+    },
+    chunkSizeWarningLimit: 600, // size in KB
+  },
 })
